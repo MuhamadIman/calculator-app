ff568815ccbd873333adafacfd432a3a464d3ab9
MuhamadIman <krbndol@gmail.com>
2020-03-14 22:17:32 +0700

    menambahkan fungsi pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 namespace Calculator
 {
 	class Calculator
-}
+{
 	static void Main(string[] args)
 	{
 
 	}
-	{
+	
 		static int penambahan(int a, int b)
+		{
 		return a+b;
+	    }
+		static int pengurangan(int a, int b)
+		{
+			return a-b;
+		}
 	}
+}
