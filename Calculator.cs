32ac912697dc5389ea047d26b4c084bd53e48c21
MuhamadIman <krbndol@gmail.com>
2020-03-14 21:45:24 +0700

    inisialisasi project

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.text;
+using System.Threading.Tasks;
+namespace Calculator
+{
+	class Calculator
+}
+	static void Main(string[] args)
+	{
+
+	}
+	{
+		
+	}
